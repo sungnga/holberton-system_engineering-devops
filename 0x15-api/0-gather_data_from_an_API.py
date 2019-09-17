1d39f818925b7244eb2b0b048affcd0b75aa4e4e
sungnga <708@holbertonschool.com>
2019-09-17 02:41:10 +0000

    Initial commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+A Python script using a REST API, for a given employee ID,
+returns information about his/her TODO list progress
+"""
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    name = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                            .format(sys.argv[1]))
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                            .format(sys.argv[1]))
+    dict_name = name.json()
+    dict_todos = todos.json()
+    comp_tasks = 0
+    tasks = 0
+    for i in dict_todos:
+        if i.get('completed') is True:
+            comp_tasks += 1
+        else:
+            tasks += 1
+    print("Employee {} is done with tasks({}/{})"
+          .format(dict_name['name'], comp_tasks, tasks))
+    for i in dict_todos:
+        if i.get('completed') == True:
+            print('\t {}'.format(i.get('title')))
