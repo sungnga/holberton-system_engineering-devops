7ae1fc4ff3c12bcb2cc31f91f0e7c645926985ce
sungnga <708@holbertonschool.com>
2019-09-17 05:03:53 +0000

    Remove newline parameter

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,13 +9,13 @@ import csv
 
 
 if __name__ == "__main__":
-    dict_users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
-                              .format(sys.argv[1])).json()
+    dict_users = requests.get('https://jsonplaceholder.\
+    typicode.com/users/{}'.format(sys.argv[1])).json()
     dict_todos = requests.get(
         'https://jsonplaceholder.typicode.com/todos?userId={}'
         .format(sys.argv[1])).json()
 
-    with open('{}.csv'.format(sys.argv[1]), 'w', newline='') as csvfile:
+    with open('{}.csv'.format(sys.argv[1]), 'w') as csvfile:
         todowriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
         for i in dict_todos:
             todowriter.writerow([i.get('userId'),
