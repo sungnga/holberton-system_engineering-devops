03eb754d1c885811f93e85a1522a42dbadf2d14b
sungnga <708@holbertonschool.com>
2019-09-17 05:10:42 +0000

    Shorten variable names

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,17 +9,15 @@ import csv
 
 
 if __name__ == "__main__":
-    dict_users = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'
-        .format(sys.argv[1])).json()
-    dict_todos = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'
-        .format(sys.argv[1])).json()
+    users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                         .format(sys.argv[1])).json()
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
+                         .format(sys.argv[1])).json()
 
     with open('{}.csv'.format(sys.argv[1]), 'w') as csvfile:
         todowriter = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        for i in dict_todos:
+        for i in todos:
             todowriter.writerow([i.get('userId'),
-                                 dict_users.get('username'),
+                                 users.get('username'),
                                  i.get('completed'),
                                  i.get('title')])
