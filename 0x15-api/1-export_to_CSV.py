b82dab6d40cf41d55bf03e7983b1b8f858f82091
sungnga <708@holbertonschool.com>
2019-09-17 04:39:19 +0000

    Initial commit

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Extend Task 0 Python script to export data in the csv format
+"""
+
+import requests
+import sys
+import csv
+
+
+if __name__ == "__main__":
+    dict_users = requests.get('https://jsonplaceholder.typicode.com/users/{}'
+                              .format(sys.argv[1])).json()
+    dict_todos = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'
+        .format(sys.argv[1])).json()
+    with open('{}.csv'.format(sys.argv[1]), 'w', newline='') as csvfile:
+        todowriter = csv.writer(csvfile,
+                                delimiter=',',
+                                quoting=csv.QUOTE_ALL)
+        for i in dict_todos:
+            todowriter.writerow([i.get('userId'),
+                                 dict_users.get('username'),
+                                 i.get('completed'),
+                                 i.get('title')])
