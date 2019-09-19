0b293763d61b01c39460b991bd79cd7855b6530e
sungnga <708@holbertonschool.com>
2019-09-19 23:06:46 +0000

    Shorten lines to 80 chars

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,8 +8,8 @@ import json
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers from the Reddit API"""
-    """Mozilla/5.0 (X11; CrOS x86_64 10066.0.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36"""
-    r = requests.get(r'http://www.reddit.com/r/{}/about.json'.format(subreddit), headers={'User-agent': 'x'})
+    r = requests.get(r'http://www.reddit.com/r/{}/about.json'
+                     .format(subreddit), headers={'User-agent': 'x'})
     json = r.json()
     subscribers = json.get('data').get('subscribers')
     if not subscribers:
