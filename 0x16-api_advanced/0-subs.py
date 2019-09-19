3d13b348682e6472a854794d94c2a5775654ca06
sungnga <708@holbertonschool.com>
2019-09-19 23:54:23 +0000

    pep8 compliance

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,13 +3,13 @@
 subscribers (not active users, total subscribers) for a given subreddit"""
 
 import requests
-import json
 
 
 def number_of_subscribers(subreddit):
     """returns the number of subscribers from the Reddit API"""
     r = requests.get(r'http://www.reddit.com/r/{}/about.json'
-                     .format(subreddit), headers={'User-agent': 'x'}, allow_redirects=True)
+                     .format(subreddit), headers={'User-agent': 'x'},
+                     allow_redirects=True)
     # print(r.status_code)
     # if r.status_code >= 400:
     #     return 0
